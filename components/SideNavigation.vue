2a30d7656ff727ac9ead31697119951242a3e529
SatoshiRC <44540173+SatoshiRC@users.noreply.github.com>
2020-03-20 00:15:58 +0900

    fix

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -89,7 +89,6 @@
 <script lang="ts">
 import Vue from 'vue'
 import { TranslateResult } from 'vue-i18n'
-import LanguageSelector from '@/components/LanguageSelector.vue'
 import MenuList from '@/components/MenuList.vue'
 
 type Item = {
