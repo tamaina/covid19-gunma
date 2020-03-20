cbe0efb49bd33efd251c287baeaea5b8c555a69a
Satoshi Ohya <conatus.11099@gmail.com>
2020-03-21 00:48:33 +0900

    linkURLを修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -87,7 +87,7 @@ export default Vue.extend({
       link: [
         {
           rel: 'canonical',
-          href: `https://covid19-gunma-d.netlify.com${this.$route.path}`
+          href: `https://covid19-gunma.netlify.com${this.$route.path}`
         }
       ],
       meta: [
@@ -116,7 +116,7 @@ export default Vue.extend({
         {
           hid: 'og:url',
           property: 'og:url',
-          content: `https://covid19-gunma-d.netlify.com${this.$route.path}`
+          content: `https://covid19-gunma.netlify.com${this.$route.path}`
         },
         {
           hid: 'og:locale',
