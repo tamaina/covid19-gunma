06fe1d75cb0540a9008d5d90fb086b7b1886bb48
Satoshi Ohya <conatus.11099@gmail.com>
2020-03-20 20:07:59 +0900

    logoを置き換えた

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -11,7 +11,7 @@
       <h1 class="SideNavigation-Heading">
         <nuxt-link :to="localePath('/')" class="SideNavigation-HeadingLink">
           <div class="SideNavigation-HeaderLogo">
-            <img src="/logo.svg" :alt="$t('群馬県')" />
+            <img src="/logo.png" :alt="$t('群馬県')" />
           </div>
           <div class="SideNavigation-HeaderLogoLinkText">
             {{ $t('新型コロナウイルス感染症') }}<br />{{ $t('対策サイト') }}
