73d7b57f57a666300904477739628b55296c6e3a
Satoshi Ohya <conatus.11099@gmail.com>
2020-03-20 17:30:05 +0900

    test

--- components/TimeBarChart.vue
+++ components/TimeBarChart.vue
@@ -11,10 +11,13 @@
     />
     <template v-slot:infoPanel>
       <data-view-basic-info-panel
-        :l-text="displayNote.text"
+        :l-text="displayInfo.lText"
         :s-text="displayInfo.sText"
         :unit="displayInfo.unit"
       />
+    </template>
+    <template v-slot:note>
+      <view-note: note="displayNote.text" />
     </template>
   </data-view>
 </template>
@@ -28,6 +31,7 @@ import { GraphDataType } from '@/utils/formatGraph'
 import DataView from '@/components/DataView.vue'
 import DataSelector from '@/components/DataSelector.vue'
 import DataViewBasicInfoPanel from '@/components/DataViewBasicInfoPanel.vue'
+import ViewNote from '@/components/ViewNote.vue'
 
 type Data = {
   dataKind: 'transition' | 'cumulative'
@@ -90,7 +94,7 @@ const options: ThisTypedComponentOptionsWithRecordProps<
   Computed,
   Props
 > = {
-  components: { DataView, DataSelector, DataViewBasicInfoPanel },
+  components: { DataView, DataSelector, DataViewBasicInfoPanel, ViewNote },
   props: {
     title: {
       type: String,
